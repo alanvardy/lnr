ccd4efa10b16f29207d7f5f854bc8a1b55439f76
Gene Pasquet <gene@pacerevenue.com>
2025-10-03 10:32:08 +0100

    Add git command to create branch when creating a issue

--- src/git.rs
+++ src/git.rs
@@ -13,3 +13,20 @@ pub fn get_branch() -> Result<String, String> {
         Err(String::from_utf8(output.stderr).unwrap())
     }
 }
+
+pub fn create_branch(branch_name: String) -> Result<String, String> {
+    let output = std::process::Command::new("git")
+        .arg("checkout")
+        .arg("-b")
+        .arg(branch_name)
+        .output()
+        .map_err(|e| e.to_string())?;
+
+    if output.status.success() {
+        String::from_utf8(output.stdout)
+            .map(|s| s.trim().to_string())
+            .map_err(|e| e.to_string())
+    } else {
+        Err(String::from_utf8(output.stderr).unwrap())
+    }
+}
